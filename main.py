b218a0b5879ba3103c2a07bae0677a1960b4d585
Marvish Subhas Chandra <mschandr@uci.edu>
2023-11-05 20:39:33 -0800

    Add files via upload

--- main.py
+++ main.py
@@ -4,29 +4,21 @@
 
 
 class fastestCarriers():
-    def weeklyStatistics(weekAvg,givenStat): #performance weekly, of overall
-        weeklyPerformance = (weekAvg * givenStat)
-        if weeklyPerformance > 20:
+    def weeklyStatistics(weekAvg,givenStat,wkcarrierAvg,rusher,passer,receiver):
+        totalPerformance = (weekAvg * givenStat) + wkcarrierAvg + rusher + passer + receiver
+        if wkcarrierAvg > 22:
             print("The weekly performance of the given ball carrier is above average.")
-        if weeklyPerformance < 18:
+        if wkcarrierAvg < 22:
             print("The weekly performance of the given ball carrier is below average.")
-        else: print("Weekly performance of the ball carrier must be between the boundaries of 18 and 25 miles per hour.")
-    def Carriers(weeklyAvg,givenStat):
-        carriersPerformance = (weeklyAvg * givenStat) > 22
-
-    def playerPosition(rusher,passer,receiver):
+        else: print("The carrier may not be available to perform.")
         if rusher > 0:
-            rusherRange = 100 > rusher > 0
             print("The rusher's range is between zero and a hundred yards")
-        else: print("The rusher did not achieve any yards to qualify.")
+        else: print("The rusher may not be available to perform.")
         if passer > 20:
-            passedRange = 100 > passer > 0
             print("The passer's range must be above twenty yards.")
-        else: print("The passer did not achieve any yards to qualify.")
         if receiver > 20:
-            receivedRange = 100 > receiver > 0
-            print("The reciever's reception must be above twenty yards")
-        else: print("The reciever did not achieve any yards to qualify.")
-
+            print("The receiver's reception must be above twenty yards.")
+        else: print("The receiver may not be available to perform.")
+        return totalPerformance
 
 
